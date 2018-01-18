18549367fd02edfa04ff900094926bd6dbb79b99
EvaFro <frolli.erin@gmail.com>
2018-01-17 20:34:41 -0800

    Completed all requierments to pass all test for Stack;

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storeIntances = {};
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storeIntances[this.count] = value;
+  this.count++;
+};
 
+Stack.prototype.pop = function() {
+  this.count--;
+  var popValue = this.storeIntances[this.count];
+  delete this.storeIntances[this.count];
+  return popValue;
+};
+
+Stack.prototype.size = function() {
+  if (this.count < 0) {
+    this.count = 0;
+  }
+  return this.count;
+};
+
+var someInstance = new Stack();
