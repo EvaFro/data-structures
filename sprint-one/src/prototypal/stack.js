16f394d3f20a9d8ef4841bd6a205c446a6c1cda4
EvaFro <frolli.erin@gmail.com>
2018-01-17 20:22:23 -0800

    Completed all requierments to pass all test for Stack.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.store = {};
+  someInstance.count = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.store[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  this.count--;
+  var popValue = this.store[this.count];
+  delete this.store[this.count];
+  return popValue;
+
+};
+
+stackMethods.size = function() {
+  if (this.count < 0) {
+    this.count = 0;
+  }
+  return this.count;
+};
