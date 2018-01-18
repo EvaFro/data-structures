8ad8ba77eadea640f6978d07b2a5e3ec45aaa7f3
EvaFro <frolli.erin@gmail.com>
2018-01-17 21:18:15 -0800

    Completed all requierments to get stack test to pass.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.store = {};
+    this.count = 0;
+  }
+  
+  pop() {
+    this.count--;
+    var popValue = this.store[this.count];
+    delete this.store[this.count];
+    return popValue;
+  }
+  
+  push(value) {
+    this.store[this.count] = value;
+    this.count++;
+
+  }
+
+  size() {
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return this.count;
+    
   }
 
 }
+
