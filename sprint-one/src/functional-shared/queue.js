9129877604f7bf5a7191336af443d533af2427b1
EvaFro <frolli.erin@gmail.com>
2018-01-17 19:42:10 -0800

    Implimented Queue to pass size tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstence = {};
+  someInstence.store = {};
+  someInstence.count = 0;
+  someInstence.countInstance = 0;
+  _.extend(someInstence, queueMethods);
+  return someInstence;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  this.count--;
+};
+
+queueMethods.size = function() {
+  if (this.count < 0) {
+    this.count = 0;
+  }
+  return this.count;
+};
 
 
