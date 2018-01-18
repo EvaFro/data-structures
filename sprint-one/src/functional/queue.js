b25824190433820ec05bf4f3296f15f342cc7e54
EvaFro <frolli.erin@gmail.com>
2018-01-17 16:20:07 -0800

    Implimented E, D, and size  -  one test fails in progress

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var dequeueValue = storage[Object.keys(storage)[0]];
+    debugger
+    delete storage[Object.keys(storage)[0]];
+    count--;
+    return dequeueValue;
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      count = 0;
+    }
+    return count;
   };
 
   return someInstance;
