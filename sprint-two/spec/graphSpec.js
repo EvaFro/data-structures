b43ad571bcaf84e5f55d363cf6d3e62662b869d5
EvaFro <frolli.erin@gmail.com>
2018-01-20 10:49:34 -0800

    Created and passed test for graph.

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -68,4 +68,12 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 5)).to.equal(true);
     expect(graph.hasEdge(5, 5)).to.equal(true);
   });
+  
+  it ('should not throw and error when removing a non-existent edge', function() {
+    graph.addNode(5);
+    graph.addNode(2);
+    expect(function() { graph.removeEdge(5, 2); }).not.throws();
+  });
+
+
 });
